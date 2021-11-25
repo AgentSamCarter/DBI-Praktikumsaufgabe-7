25835c9748b119ec9f8313dffb583430729c4dd4
Anne Loettert <anne.loettert@studmail.w-hs.de>
2021-11-25 08:16:53 +0100

    Version Anne 2.0

--- Praktikum7/src/test.java
+++ Praktikum7/src/test.java
@@ -1,44 +1,42 @@
 import com.mysql.cj.xdevapi.Result;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class test
 {
     public static void main(String[] args) throws SQLException
     {
+           Scanner sc =new Scanner(System.in);
+        System.out.println("Wie groß ist n?");
+           int n =sc.nextInt();
+        Connection conn = create_Connection.createConnection();
+        drop_Table.drop(conn);
+
+
+       create_Table.create(conn);
+
+
         long start = System.currentTimeMillis();
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:10","dbineu","123");
-
-        Statement st = conn.createStatement();
-
-        datenbank(10,conn);
-
+       // fill_Database.datenbank(n,conn);
+        loadDataLocalInfileMySQL.load_Data(conn,n);
 
         long ende= System.currentTimeMillis();
+        //  delete_Database.delete(conn);
 
         System.out.println("Dauer der Anwendung  "+(ende-start));
 
+        conn.close();
+        sc.close();
+
     }
 
-    static void datenbank (int i, Connection conn)
-    {
-        for ( i = 1; i <=1000 ; i++)
-        {
-            int test =i;
 
-            try {
-                PreparedStatement queryStatement = conn.prepareStatement("INSERT INTO dbi.branches VALUES ("+test+", 'Anne', 1000, 'abcdefghijklmnopqrstuvwxyz');");
-
-                queryStatement.executeUpdate(); //Executes the query
-                queryStatement.close();
-
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        }
 
 
 
-     }
-    }
-}
+
+
+
+
